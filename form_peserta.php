989d9b55337c0773873cc1b31b5f413c5405223d
arifianjuari <arifianjuari@gmail.com>
2025-06-10 12:10:22 +0700

    tes3

--- form_peserta.php
+++ form_peserta.php
@@ -18,7 +18,6 @@ require_once 'includes/functions.php';
 require_once 'includes/validation.php';
 require_once 'includes/ocr.php';
 // tes perubahan
-// tes 2
 // Page title
 $pageTitle = 'HUT Bhayangkara ke-79 - Pendaftaran Peserta';
 
